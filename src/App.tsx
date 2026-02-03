db09a40dfca0914e910b3365a3778915c9167793
rianprei <lucaguerian@outlook.com>
2026-02-03 19:30:55 -0300

    remove arquivas não utilizados.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-
-
-  return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
-  )
+  return <div>
+    
+  </div>;
 }
 
-export default App
+export default App;
